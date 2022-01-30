c55c3db1c4a2012236c217de352bd75c9f63e62e
mateu <mateusz.wygoda>
2022-01-30 22:20:00 +0100

    Add contructor and toString method to Car.java

---
+++ src/Car.java
@@ -0,0 +1,29 @@
+public class Car {
+    String brand;
+    String model;
+    int productionYear;
+    int engine;
+    int doorsCount;
+    String color;
+
+    Car(String brand, String model, int productionYear, int engine, int doorsCount, String color){
+        this.brand = brand;
+        this.model = model;
+        this.productionYear = productionYear;
+        this.engine = engine;
+        this.doorsCount = doorsCount;
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", productionYear=" + productionYear +
+                ", engine=" + engine +
+                ", doorsCount=" + doorsCount +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
