7bd0f818d926c5d43f940a95a908efcb11743a51
mateu <mateusz.wygoda>
2022-01-30 22:23:31 +0100

    Add methods from class Car.java to Shop.java

--- src/Shop.java
+++ src/Shop.java
@@ -1,5 +1,17 @@
+import java.util.logging.SocketHandler;
+
 public class Shop {
     public static void main(String[] args) {
+        final String appName = "Car dealer v0.4";
+
+        Car car1 = new Car("Mazda","626",2004,1600,5,"yellow");
+        Car car2 = new Car("Kia","Rio",2016,1300,5,"gray");
+
+
+        System.out.println(appName);
+        System.out.println("Available Cars:");
+        System.out.println(car1);
+        System.out.println(car2);
 
     }
 }
