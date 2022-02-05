97a29136fddaa9720d49d1ad20cc407d04001b1f
mateu <mateusz.wygoda>
2022-02-05 22:42:44 +0100

    Add table to Shop.java

--- src/pl/carshop/app/Shop.java
+++ src/pl/carshop/app/Shop.java
@@ -4,16 +4,18 @@ import pl.carshop.model.Car;
 
 public class Shop {
     public static void main(String[] args) {
-        final String appName = "Car dealer v0.5";
+        final String appName = "Car dealer v0.7";
 
-        Car car1 = new Car("Mazda","626",2004,1600,5,"yellow");
-        Car car2 = new Car("Kia","Rio",2016,1300,5,"gray");
+        Car [] car = new Car[100];
+
+        car[0] = new Car("Mazda","626",2004,1600,5,"yellow");
+        car[1] = new Car("Kia","Rio",2016,1300,5,"gray");
 
 
         System.out.println(appName);
         System.out.println("Available Cars:");
-        System.out.println(car1);
-        System.out.println(car2);
+        System.out.println(car[0]);
+        System.out.println(car[1]);
 
     }
 }
